3de72e3955649b4a4ea41cbad449c608d56042d5
Jia <49358172+craftslab@users.noreply.github.com>
2024-04-01 07:53:43 +0000

    feat(scripts): use bazelist as bazel launcher for gh actions

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,6 +6,6 @@ export PATH=$PWD/depot_tools:$PATH
 # dumpstats: bazel-bin/cmd/dumpstats/dumpstats_/dumpstats
 # reproxy: bazel-bin/cmd/reproxy/reproxy_/reproxy
 # rewrapper: bazel-bin/cmd/rewrapper/rewrapper_/rewrapper
-bazel-6.3.2 build --config=clangscandeps //cmd/...
+bazelisk build --config=clangscandeps //cmd/...
 
 # scandeps_server: https://chrome-infra-packages.appspot.com/p/infra/rbe/client/linux-amd64
--- scripts/install.sh
+++ scripts/install.sh
@@ -2,10 +2,9 @@
 
 # Install Bazel
 sudo apt install apt-transport-https curl gnupg -y
-curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
-sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
-sudo apt update && sudo apt install -y bazel-6.3.2
+curl -L https://github.com/bazelbuild/bazelisk/releases/download/v1.19.0/bazelisk-linux-amd64 -o bazelisk
+chmod +x bazelisk
+sudo mv bazelisk /usr/local/bin
 
 # Install depot_tools
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools --depth=1
